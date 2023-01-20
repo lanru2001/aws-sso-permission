632aee78edb48ec04f4bb5871670a02ecd9186a9
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2023-01-20 12:40:35 -0600

    Create s3-lambda-loader.tf

---
+++ s3-lambda-loader.tf
@@ -0,0 +1,54 @@
+module "aws_sso_s3_bi_lambda_loader" {
+    source  = "../.."
+    version = "0.1.3"
+    inline_create = "yes"
+
+
+  name               = "sso_s3_bi_lambda_loader"
+  description        = "S3 creditera-assets full access"
+  group_display_name = "AWS S3_BI_Lambda-Loader"
+  session_duration   = "PT1H"
+
+  account_ids = [
+    "374770275786"
+  ]
+
+  managed_policy_arns = []
+
+  inline_policy = data.aws_iam_policy_document.sso_s3_bi_lambda_loader.json
+
+  tags = {
+    managed_by = "Terraform"
+  }
+}
+
+data "aws_iam_policy_document" "sso_s3_bi_lambda_loader" {
+    statement {
+       sid = "Stmt1430942563000"
+       effect = "Allow"
+       actions = [
+                "s3:GetBucketLocation",
+                "s3:GetAccountPublicAccessBlock",
+                "s3:GetBucketPublicAccessBlock",
+                "s3:GetBucketPolicyStatus",
+                "s3:GetBucketAcl",
+                "s3:ListAccessPoints",
+                "s3:ListAllMyBuckets"
+            ]
+       resources=  [ "arn:aws:s3:::*" ]
+        }
+    statement {
+       effect =  "Allow"
+       actions = [ "s3:ListBucket" ]
+       resources = [ "arn:aws:s3:::bi-lambda-loader"]
+        }
+    statement {
+        effect = "Allow"
+        actions = [ "s3:*" ]
+        resources = [
+                "arn:aws:s3:::bi-lambda-loader",
+                "arn:aws:s3:::bi-lambda-loader/*"
+            ]
+        }
+    
+}
